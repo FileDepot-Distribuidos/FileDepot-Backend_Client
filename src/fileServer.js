d05ecb62c0a3a6a280572354547a7f8388307d88
jreyess11 <josegabrielreyessolanojr@gmail.com>
2025-04-27 11:39:39 -0500

    arreglo conexiones locales

--- src/fileServer.js
+++ src/fileServer.js
@@ -6,22 +6,37 @@ dotenv.config();
 
 const app = express();
 
-// middleware global
-app.use(cookieParser()); // <-- ya deberías tenerlo
-app.use(cors({
-    origin: 'http://localhost:8080', // o el host de tu frontend
-    credentials: true, // <--- ¡esto es lo que falta!
-}));
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }))
+// Obtenemos la LAN permitida
+const allowedLan = process.env.ALLOWED_LAN; // ejemplo: '192.168.20'
 
-// Importar las rutas
+app.use(cookieParser());
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true); // para Postman o curl
+        
+        // Permitimos localhost y cualquier IP que empiece con 192.168.20.*
+        const regexLan = new RegExp(`^http://${allowedLan}\\.\\d{1,3}:8080$`);
+
+        if (regexLan.test(origin) || origin === 'http://localhost:8080') {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+}));
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+// Importar rutas
 import routes from './routes/routes.js';
 app.use('/api', routes);
 
-// Iniciar el servidor
-const port = process.env.FILES_PORT || 3000;
-const host = process.env.FILES_HOST || 'localhost';
+// Iniciar servidor
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || 'localhost';
 
 app.listen(port, () => {
     console.log(`Servidor corriendo en http://${host}:${port}`);
